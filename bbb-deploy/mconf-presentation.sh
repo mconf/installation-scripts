8912fe3d367b33383c536ad44c47d63a3d3bc8a5
Felipe Cecagno <fcecagno@gmail.com>
2012-04-27 15:00:52 -0300

    uploading a default presentation for the Mconf servers, and a script to copy and set the presentation as the default presentation for that server

---
+++ bbb-deploy/mconf-presentation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo cp mconf-default.pdf /var/www/bigbluebutton-default/
+sudo sed -i "s:\(beans.presentationService.defaultUploadedPresentation\).*:\1=${bigbluebutton.web.serverURL}/mconf-default.pdf:g" /var/lib/tomcat6/webapps/bigbluebutton/WEB-INF/classes/bigbluebutton.properties
+
